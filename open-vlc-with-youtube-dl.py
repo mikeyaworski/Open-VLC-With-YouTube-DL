3a9e832092cef84f93f21964ad61a6333fe91439
Michael Yaworski <mikeyaworski@hotmail.com>
2024-10-21 02:34:14 -0400

    Fix: Allow audio stream to be omitted

--- open-vlc-with-youtube-dl.py
+++ open-vlc-with-youtube-dl.py
@@ -6,21 +6,30 @@ while True:
   try:
     url = input('URL: ')
     subprocess.call(f'yt-dlp -F {url}')
+
     video_format = input('(Optional) Choose VIDEO format: ') or 'best'
-    audio_format = input('(Optional) Choose AUDIO format: ') or 'bestaudio'
+    audio_format = input('(Optional) Choose AUDIO format: ')
+
     # Two separate commands since the format of --get-url sometimes returns multiple results for the video format,
     # which means doing --format={video_format}+{audio_format} is difficult to parse
     video_stream_url = subprocess.check_output(f'yt-dlp --get-url --format {video_format} {url}')
     video_stream_url = video_stream_url.decode('utf-8').split('\n')[0]
-    audio_stream_url = subprocess.check_output(f'yt-dlp --get-url --format {audio_format} {url}')
-    audio_stream_url = audio_stream_url.decode('utf-8').split('\n')[0]
-    subprocess.Popen([
+
+    args = [
       vlc_path,
       '--meta-title=youtube-dl',
       video_stream_url,
-      "--input-slave",
-      audio_stream_url
-    ])
+    ]
+
+    if audio_format:
+      audio_stream_url = subprocess.check_output(f'yt-dlp --get-url --format {audio_format} {url}')
+      audio_stream_url = audio_stream_url.decode('utf-8').split('\n')[0]
+      args.extend([
+        "--input-slave",
+        audio_stream_url
+      ])
+
+    subprocess.Popen(args)
     break
   except Exception as e:
     print(e)
